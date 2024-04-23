20c070ee0a383e6b7f909d2f2ead6bbb110eba76
Pablo Nocera <p.nocerar@gmail.com>
2024-04-23 00:16:01 -0300

    se arma el navbar con bootstrap

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,16 +2,51 @@ import CartWidget from "./CartWidget";
 import "./NavBar.css";
 const NavBar = () => {
   return (
-    <div className="nav-bar">
-      <div>
-        <img src="/src/assets/logo_navbar.png" alt="" />
-      </div>
-      <ul className="list-store">
-        <li>Basicos</li>
-        <li>Extras</li>
-        <li>Contactanos</li>
-      </ul>
-      <CartWidget />
+    <div>
+      <nav
+        className="navbar bg-dark navbar-expand-lg bg-body-tertiary"
+        data-bs-theme="dark"
+      >
+        <div className="container-fluid">
+          <img src="/src/assets/logo_navbar.png" alt="" />
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Inicio
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" href="#">
+                  Basicos
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" href="#">
+                  Extras
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" href="#">
+                  Contactanos
+                </a>
+              </li>
+            </ul>
+          </div>
+          <CartWidget />
+        </div>
+      </nav>
     </div>
   );
 };
